9a1edccf6df682c1c36882b1bbd48e4f61fd3261
intrvertmichael <michaelpaguay@gmail.com>
2024-11-13 19:08:00 -0500

    connected to Neon db

--- app/db.js
+++ app/db.js
@@ -1,9 +1,10 @@
 import postgres from "postgres"
 
-export default postgres("postgres://localhost", {
-  host: "", // Postgres ip address[s] or domain name[s]
-  port: 5432, // Postgres server port[s]
-  database: "michael", // Name of database to connect to
-  username: "", // Username of database user
-  password: "", // Password of database user
+export default postgres("postgres://", {
+  host: process.env.PGHOST,
+  port: process.env.PGPORT, // Postgres server port[s]
+  database: process.env.PGDATABASE, // Name of database to connect to
+  username: process.env.PGUSER, // Username of database user
+  password: process.env.PGPASSWORD, // Password of database user
+  ssl: "require",
 })
